186acb907ac50128cac3d94dbdc7e7e612c62449
Nikolas-Loukas Oikonomou <122741045+it21967@users.noreply.github.com>
2023-09-15 14:51:29 +0300

    Add files via upload

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,40 @@
+pipeline {
+    agent any
+
+
+    stages {
+        stage('Build') {
+            steps {
+                // Get some code from a GitHub repository
+                git branch: 'main', url: 'https://github.com/it21967/test-jenkins.git'
+
+                
+            }
+        }
+        
+        stage('Test') {
+            steps {
+                sh '''
+                    python3 -m venv myvenv
+                    source myvenv/bin/activate
+                    pip install -r requirements.txt
+                    cd myproject
+                    cp myproject/.env.example myproject/.env
+                    ./manage.py test'''
+            }
+        }
+        stage('install ansible prerequisites') {
+            steps {
+                sh '''
+                    ansible-galaxy install geerlingguy.postgresql
+                '''
+
+                sh '''
+                    mkdir -p ~/workspace/ansible-project/files/certs
+                    cd ~/workspace/ansible-project/files/certs
+                    openssl req -x509 -newkey rsa:4096 -keyout server.key -out server.crt -days 365 --nodes -subj '/C=GR/O=myorganization/OU=it/CN=myorg.com'
+                '''
+            }
+        }
+    }
+}
